8526fb7c97fe194b963159c4753cd9f2c7a0361e
jior2008 <jior2008@163.com>
2020-01-19 12:38:11 +0800

    update

--- src/main/java/com/glaf/core/util/JacksonUtils.java
+++ src/main/java/com/glaf/core/util/JacksonUtils.java
@@ -19,15 +19,10 @@
 package com.glaf.core.util;
 
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -67,35 +62,7 @@ public class JacksonUtils {
 		return objectMapper.readValue(jsonStr, Map.class);
 	}
 
-	/**
-	 * json string convert to map with javaBean
-	 */
-	public static <T> Map<String, T> json2map(String jsonStr, Class<T> clazz)
-			throws Exception {
-		Map<String, Map<String, Object>> map = objectMapper.readValue(jsonStr,
-				new TypeReference<Map<String, T>>() {
-				});
-		Map<String, T> result = new HashMap<String, T>();
-		for (Entry<String, Map<String, Object>> entry : map.entrySet()) {
-			result.put(entry.getKey(), map2pojo(entry.getValue(), clazz));
-		}
-		return result;
-	}
-
-	/**
-	 * json array string convert to list with javaBean
-	 */
-	public static <T> List<T> json2list(String jsonArrayStr, Class<T> clazz)
-			throws Exception {
-		List<Map<String, Object>> list = objectMapper.readValue(jsonArrayStr,
-				new TypeReference<List<T>>() {
-				});
-		List<T> result = new ArrayList<T>();
-		for (Map<String, Object> map : list) {
-			result.add(map2pojo(map, clazz));
-		}
-		return result;
-	}
+	 
 
 	/**
 	 * map convert to javaBean
